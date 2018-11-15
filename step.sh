d5a648d8574067ff6fc1bc2a527fb7199460deca
Daniel McConville <dan@dgmtechnology.com>
2018-11-15 19:10:53 +1030

    Add step

--- step.sh
+++ step.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 set -ex
 
-echo "This is the value specified for the input 'example_step_input': ${example_step_input}"
+if [ -z ${directory_to_zip+x} ]; then
+	echo "directory_to_zip environment variable is not set, make sure you run this step after the xcode-archive step."
+	exit 1
+fi
+if [ -z ${include_directory+x} ]; then
+	echo "include_directory environment variable is not set, make sure you run this step after the xcode-archive step."
+	exit 1
+fi
+if [ ! -e ${directory_to_zip} ]; then
+	echo "Could not find ${directory_to_zip} directory, please make sure it exists."
+	exit 1
+fi
 
-#
-# --- Export Environment Variables for other Steps:
-# You can export Environment Variables for other Steps with
-#  envman, which is automatically installed by `bitrise setup`.
-# A very simple example:
-envman add --key EXAMPLE_STEP_OUTPUT --value 'the value you want to share'
-# Envman can handle piped inputs, which is useful if the text you want to
-# share is complex and you don't want to deal with proper bash escaping:
-#  cat file_with_complex_input | envman add --KEY EXAMPLE_STEP_OUTPUT
-# You can find more usage examples on envman's GitHub page
-#  at: https://github.com/bitrise-io/envman
+if [ ${include_directory} == false ]; then
+	cd ${directory_to_zip}
+	zip -r ${directory_to_zip}.zip ./*
+else
+	zip -r ${directory_to_zip}.zip ${directory_to_zip}
+fi
 
-#
-# --- Exit codes:
-# The exit code of your Step is very important. If you return
-#  with a 0 exit code `bitrise` will register your Step as "successful".
-# Any non zero exit code will be registered as "failed" by `bitrise`.
+envman add --key ZIP_FILE --value $(pwd)/temp.zip
